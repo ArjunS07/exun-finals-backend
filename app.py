92810da7e0277388ca9cf8da91a4256a16414d57
ArjunS07 <arjun.sharma07@outlook.com>
2022-11-11 22:01:09 +0530

    Add TODOs

--- app.py
+++ app.py
@@ -3,11 +3,21 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
+from matching_utils import get_best_available_user_match
+from audio_utils import download_audio, transcribe_audio
+
+@app.route('/transcribe-audio', methods=['POST'])
+def transcribe_audio(request):
+    '''Downloads the audio file at the given url and returns the string transcribed by openAI whisper'''
+    url = request.form['url']
+    path = download_audio(url)
+    transcribed = transcribe_audio(path)
+    
+    # Returns transcribed for now, but should instead update the firestore database with the transcribed text
+    return transcribed
 
 
-@app.route('/about/')
-def about():
-    return '<h3>This is a Flask web application.</h3>'
+@app.route('/match-user', methods=['POST'])
+def match_user(request):
+    '''Finds the closest match for a user and opens a chatorom with them.'''
+    # TODO: implement this function
---
+++ matching_utils.py
@@ -0,0 +1,14 @@
+model_path = "model.pickle"
+# TODO: Open the 
+
+def user_match_score(user_1: dict, user_2: dict):
+    '''Returns the match score between two users using the model'''
+    pass
+
+def ranked_users(target_user: dict, users: list):
+    '''Returns a list of all users ranked by match score'''
+    pass
+
+def get_best_available_user_match(target_user: dict, users: list):
+    '''Returns the best available match for a user from a list of users'''
+    pass
