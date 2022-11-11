2ac9ea4b210a1cb7b94b1a8561f43afed1cff40d
ArjunS07 <arjun.sharma07@outlook.com>
2022-11-11 22:02:17 +0530

    Clear up comments

--- audio_utils.py
+++ audio_utils.py
@@ -7,8 +7,8 @@ def download_audio(url: str) -> str:
     '''Downloads the audio file at the given url and returns the path to the downloaded file'''
     # TODO
 
-def transcribe_audio(path: str) -> str:
-    '''For an audio file at the given path, uses openAI whisper to transcribe it and returns the string ''' 
+def transcribe_audio(path: str, target_lang: str) -> str:
+    '''For an audio file at the given path, uses openAI whisper to translate and transcribe it. 
+    Returns the string of the transcription.''' 
 
     # TODO: implement this function
-    
