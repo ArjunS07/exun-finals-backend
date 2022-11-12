db472e8b90e066d55cfa49fc6c06aaf2c4fe2fb5
ArjunS07 <arjun.sharma07@outlook.com>
2022-11-12 11:37:36 +0530

    Add utils

--- audio_utils.py
+++ audio_utils.py
@@ -2,20 +2,31 @@
 # Look at https://github.com/openai/whisper
 import requests
 import os
+import whisper
 
 model = whisper.load_model("base")
 
 def download_audio(url: str) -> str:
     '''Downloads the audio file at the given url and returns the path to the downloaded file'''
     response = requests.get(url)
-    last_10 = url[-10:]
-    thefile = open(last_10, "wb").write(response.content)
-    path = str(os.path.dirname(thefile))
+    print(url)
+    filename = url.split('/')[-1]
+    download_path = f"audio_uploads/{filename}"
+    thefile = open(download_path, "wb").write(response.content)
+    return download_path
 
-    # TODO
+model = whisper.load_model("small")
+def transcribe_audio(url: str, target_lang: str = 'en') -> str:
+    path = download_audio(url)
+    audio = whisper.load_audio(path)
+    audio = whisper.pad_or_trim(audio)
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+    _, probs = model.detect_language(mel)
+    options = whisper.DecodingOptions(fp16=False, language=target_lang)
+    result = whisper.decode(model, mel, options)
+    return (result.text)
 
-def transcribe_audio(path: str, target_lang: str) -> str:
-    '''For an audio file at the given path, uses openAI whisper to translate and transcribe it. 
-    Returns the string of the transcription.''' 
-
-    # TODO: implement this function
+if __name__=='__main__':
+    url = "https://firebasestorage.googleapis.com/v0/b/exun-finals-c3c44.appspot.com/o/audioMessages%2FJamal%20Says%20'Every%2060%20Seconds%20In%20Africa%2C%20A%20Minute%20Passes'.-7Zm1hPbmzPw.m4a?alt=media&token=523c30ee-f25e-4be0-a12b-1532d9df0f56.m4a"
+    transcribed = transcribe_audio(url)
+    print(transcribed)
--- sentiment_utils.py
+++ sentiment_utils.py
@@ -1,24 +1,41 @@
 """
 TODO: Fill in this file
 - Use VADERSentiment to get the sentiment of a string
-- 
-
 
 """
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+sid_obj = SentimentIntensityAnalyzer()
+
+TOO_NEG_THRESHOLD = -0.6
 
 def get_str_sentiment(text: str) -> float:
-    """Returns the sentiment of the text as a string"""
+    """Returns the sentiment of the text """
+
+    sentiment_dict = sid_obj.polarity_scores(text)
+    return sentiment_dict['compound']
     # TODO
 
 def get_avg_list_sentiment(text_list: list) -> float:
-    """Returns the average sentiment of all messages in a list"""
-    # TODO
+    """Returns the average sentiment of all messages in a list using get_str_sentiment"""
+    for x in text_list:
+        sum_sentiment += get_str_sentiment(x)
+
+    avg = sum_sentiment/len(text_list)
+    return avg
 
 
 def get_user_messages(user_id: str) -> list:
     '''Gets all of the messages sent by a user'''
     # Don't worry about this for now
+    # TODO: implement this function
     
 def get_avg_user_sentiment(user_id: str) -> float:
-    '''Gets the aveage sentiment of the messages sent by a user'''
-    # Don't worry about this for now
+    '''Gets the averge sentiment of the messages sent by a user'''
+    messages = get_user_messages(user_id)
+    user_sentiment = get_avg_list_sentiment(messages)
+    return user_sentiment
+    
+def should_flag_user(user_id: str) -> bool:
+    '''Returns true if the user's sentiment is too negative'''
+    avg_sentiment = get_avg_user_sentiment(user_id)
+    return avg_sentiment < TOO_NEG_THRESHOLD
