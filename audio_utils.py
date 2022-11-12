bb861e758e5c99b71b5ddf7e6f11083aeef9f13e
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2022-11-12 10:50:49 +0530

    audio utils
    Co-authored-by: Shreedhar Raj 6355 [TSRS-ML] <Shreedhar.Raj6355@tsrs.org>

--- audio_utils.py
+++ audio_utils.py
@@ -1,9 +1,17 @@
 # TODO: Install and import whisper
 # Look at https://github.com/openai/whisper
+import requests
+import os
+
 model = whisper.load_model("base")
 
 def download_audio(url: str) -> str:
     '''Downloads the audio file at the given url and returns the path to the downloaded file'''
+    response = requests.get(url)
+    last_10 = url[-10:]
+    thefile = open(last_10, "wb").write(response.content)
+    path = str(os.path.dirname(thefile))
+
     # TODO
 
 def transcribe_audio(path: str, target_lang: str) -> str:
